8c8eab8f1412918c92d34a8ce4e26cca4742dfb8
Ryan <ryan.reese11@gmail.com>
2019-05-07 14:47:20 -0600

    fixed form

--- src/components/Main.js
+++ src/components/Main.js
@@ -78,7 +78,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Contact</h2>
-          <form name="form 1" method="post" action="#" data-netlify="true">
+          <form name="contact" method="post" action="#" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
